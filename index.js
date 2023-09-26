61278d3b04e7f8b7639187e624c5be578d566b56
Wasie123 <127023454+rens20@users.noreply.github.com>
2023-09-26 20:11:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,84 @@
-let searchInput = document.querySelector(".search-box");
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
-let apiData = {
-  url: `https://api.openweathermap.org/data/2.5/weather?q=`,
-  key: `9b81d3e4265e2d1a5b8b73f1591ec139`,
-};
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
 
-function getData(countryName) {
-  fetch(`${apiData.url}${countryName}&appid=${apiData.key}`)
-    .then(res => res.json())
-    .then(data => {
-      showData(data);
-      searchInput.value = "";
-    })
-    .catch(res => {
-      alert("There is no such country (404)");
-      searchInput.value = "";
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
     });
-};
 
-function showData(data) {
-  let  cityDiv = document.querySelector(".city"),
-       dateDiv = document.querySelector(".date"),
-       tempDiv = document.querySelector(".temp"),
-       weatherDiv = document.querySelector(".weather").style.color = "";
-       highLowDiv = document.querySelector(".hi-low").style.color = "red";
-  // console.log(data.sys.country);
-  cityDiv.innerHTML = `${data.name}, ${data.sys.country}`;
-  dateDiv.innerHTML = showDate();
-  tempDiv.innerHTML = `${Math.floor(data.main.temp - 273.15)} °c`;
-  weatherDiv.innerHTML = `${data.weather[0].main}`;
-  highLowDiv.innerHTML = `${Math.floor(data.main.temp_max - 273.15)} °c / ${Math.floor(data.main.temp_min - 273.15)} °c`;
-};
-
-function showDate() {
-     
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let now = new Date();
-
-  let day = days[now.getDay()];
-  let month = months[now.getMonth()];
-  let year = now.getFullYear();
-  let date = now.getDate();
-
-  return `${day} ${date} ${month} ${year}`;
-
-};
-//style.color = 'white';
-function eventSearchInput(event) {
-  if (event.keyCode === 13) {
-    const searchInputValue = searchInput.value;
-    getData(searchInputValue);
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well `;
+      form.reset();
+      input.focus();
+      return;
+    }
   }
-};
-searchInput.addEventListener("keypress", eventSearchInput);
-  
-  
-    
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city ";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
